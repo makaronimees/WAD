bcf7a3e8a7a89839b78aaa4ecb3e62ed42e17520
Kriset <kriset@laiuste.ee>
2025-12-09 13:48:20 +0200

    typos and stuff :p

--- wad/src/components/LoginForm.vue
+++ wad/src/components/LoginForm.vue
@@ -2,11 +2,12 @@
 
 <div class="loginform">
 <form id="logIn" @submit="checkPassword"> 
-    <p>Email: <input id="email" v-model="email" type="text" ></p>
-    <p>Password: <input id="password" v-model="password" type="text"  minlength="8" maxlength="15"></p>
+    <p>Email: <input id="email" v-model="email" type="text" required></p>
+    <p>Password: <input id="password" v-model="password" type="text" required minlength="8" maxlength="15"></p>
     
     <p v-if="errors.length">
-    <b>Login unsuccessful! Please check your email or password</b>
+    <b>Login unsuccessful! <br>
+        Please check your email and password</b>
   </p>
     <button type="submit" value="Submit">Login</button>
     Or
@@ -34,60 +35,7 @@ export default {
         this.$router.push('/signup')
     },
     checkPassword(e) {
-      this.errors = [];//array to save all the problems
-    
-      if(!(this.password.charAt(0)==this.password.charAt(0).toUpperCase())) {
-        this.errors.push("First letter is not capitalized!");
-      }
-      //making variables for saving info about conditions
-      let twoLower=0;
-      let oneUpper=false;
-      let lowerDash=false;
-      let numbers=false;
-    
-      for(let i=0;i<this.password.length;i++){
-        if((twoLower>=2 && oneUpper && lowerDash && numbers)){
-          break;
-        }
-        
-        else {
-          let element=this.password.charAt(i);//saving it as a variable so its easier to read
-    
-          if(!isNaN(element)){//first checking if its a number, so it hopefully doesnt try to convert numbers to uppercase etc
-            numbers=true;
-          }
-
-          else{
-            if((element.localeCompare("_")==0)){
-              lowerDash=true;
-            }
-            else{
-              if((element.localeCompare(element.toUpperCase())==0)){
-                oneUpper=true;
-              }
-
-              if((element.localeCompare((element.toLowerCase()))==0)){
-                twoLower++;
-              }
-            }
-          }
-        }
-      
-      }
-      //now lets let the user know why their passowrd sucks
-      if((twoLower<2)){
-        this.errors.push("Does not contain 2 lowercase letters!");
-      }
-      if(!oneUpper){
-        this.errors.push("Does not contain 1 uppercase letter!");
-      }
-      if(!lowerDash){
-        this.errors.push('Does not contain "_" character!');
-      }
-      if(!numbers){
-        this.errors.push("Does not contain 1 number!");
-      }
-      
+      //check the database for the credentials
     }
   }
 
